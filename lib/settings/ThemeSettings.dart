d8ff0e8a05dd35b9c64a0f9727a3e6aefc7760b2
Ahmed Rafik Radwan <ahmedrafik_maaar@outlook.com>
2021-07-17 14:19:17 +0200

    feat: Add navigation bar to standard template

--- lib/settings/ThemeSettings.dart
+++ lib/settings/ThemeSettings.dart
@@ -4,7 +4,11 @@ abstract class ThemeSettings {
   // Colors
   static const MaterialColor colorSwatch = Colors.blue;
   static const Color colorText = Color(0xff000000);
+  static const Color colorNavigation = Color(0xffffffff);
 
   // Font sizes
   static const double fontSizeNormal = 16;
+
+  // spacing
+  static const double spaceNotch = 5;
 }
--- lib/widgets/templates/StandardTemplate.dart
+++ lib/widgets/templates/StandardTemplate.dart
@@ -1,26 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:lost_children_frontend/settings/ThemeSettings.dart';
 
 class StandardTemplate extends StatelessWidget {
   final String title;
   final Widget body;
-  final FloatingActionButton? floatingActionButton;
 
   const StandardTemplate({
     Key? key,
     required this.body,
     required this.title,
-    this.floatingActionButton,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(title),
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: body,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.camera_alt),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BottomAppBar(
+        color: ThemeSettings.colorNavigation,
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 5,
+        child: Padding(
+          // make sure icons doesn't slip under the notch
+          padding: const EdgeInsets.only(left: 10, right: 90),
+          child: Row(
+            //children inside bottom appbar
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              IconButton(
+                icon: const Icon(
+                  Icons.search,
+                  color: ThemeSettings.colorText,
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(
+                  Icons.people,
+                  color: ThemeSettings.colorText,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
-        body: body,
-        floatingActionButton: floatingActionButton);
+      ),
+    );
   }
 }
